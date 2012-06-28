4ea1dc6ad50ec074ff05c371cf0bfe3986f197c8
Valentine <1@1.com>
2012-06-28 10:29:57 +0300

    First commit. One spec in progress

---
+++ TicTakToe.Specs/TicTacToeStepDefinition.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace TicTakToe.Specs
+{
+    public static class TableRowsExtension
+    {
+        public static GameField ToGameField(this TableRows theRows)
+        {
+            var gameField = new GameField();
+
+        }
+    }
+
+    public class GameField
+    {
+    }
+
+    [Binding]
+    public class TicTacToeStepDefinition
+    {
+        [Given(@"field:")]
+        public void GivenField(Table table)
+        {
+            var rows = table.Rows.ToGameField();
+        }
+
+        [Then(@"the result should be:")]
+        public void ThenTheResultShouldBe(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"try to update field with:")]
+        public void WhenTryToUpdateFieldWith(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+
+    public class GameFieldInfo
+    {
+        public GameFieldInfo()
+        {
+        }
+    }
+}
---
+++ TicTakToe.Specs/TikTacToe.feature
@@ -0,0 +1,20 @@
+﻿Feature: TicTacToe
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Should not be able to replace tic with tak
+	Given field:
+    | _0_ |
+    | ___ |
+    | ___ |
+	When try to update field with:
+    | _x_ |
+    | ___ |
+    | ___ |
+	Then the result should be:
+    | _0_ |
+    | ___ |
+    | ___ |
+
